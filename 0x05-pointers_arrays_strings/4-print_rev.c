3a8ecb3bfa3579b1fd556d8028df372b86c06167
vince-emroz <emroz44@gmail.com>
2022-09-19 15:37:30 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,22 +5,15 @@
 /**
  * print_rev - a function that prints a string, in reverse
  * fcounter is to first count to end, n is to count back
- * @s: str input
- * Return: string in reverse
+ * @s: String to reverse
+ * Return: nothing
  */
+
 void print_rev(char *s)
 {
-	int fcounter = 0;
-	int i, n;
+	int len - strlen(s);
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		fcounter++;
-	}
-
-	for (n = (fcounter - 1); n >= 0; n--)
-	{
-		_putchar(s[n]);
-	}
-	_putchar('\n');
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
 }
