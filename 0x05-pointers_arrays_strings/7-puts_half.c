cc773d1e296a28d18e04db3df50a5c539e92a6fd
vince-emroz <emroz44@gmail.com>
2022-09-20 12:03:09 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,21 +8,21 @@
  */
 void puts_half(char *str)
 {
-	int count = 0, i;
+	int index, half;
 
-	while (count >= 0)
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+
+	half = index / 2;
+
+	if (index % 2 == 1)
+		half++;
+
+	while (half < index)
 	{
-		if (str[count] == '\0')
-			break;
-		count++;
+		_putchar(str[half]);
+		half++;
 	}
-
-	if (count % 2 == 1)
-		i = count / 2;
-	else
-		i = (count - 1) / 2;
-
-	for (i++; i < count; i++)
-		_putchar(str[i]);
 	_putchar('\n');
 }
